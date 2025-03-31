5b665118974190a830132cd2edc7553673d6be54
belinore <belinore@student.42lisboa.com>
2025-03-31 21:51:02 +0100

    fixed

--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -6,52 +6,13 @@
 /*   By: belinore <belinore@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 19:45:21 by belinore          #+#    #+#             */
-/*   Updated: 2025/03/21 19:32:56 by belinore         ###   ########.fr       */
+/*   Updated: 2025/03/31 21:43:30 by belinore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	print_char(char c, int *print_len);
-static int	print_string(char *str, int *print_len);
-static int	print_number(int nbr, char c, int *print_len);
-static int	print_hexadec(size_t nbr, char format, int *print_len);
-static int	print_float(double nbr, int *print_len);
-
-
-int	printer(va_list args_ptr, const char *str, int *print_len)
-{
-	int	i;
-	int	x;
-
-	i = 0;
-	x = 0;
-	while (str[i])
-	{
-		if (str[i] != '%' || (str[i] == '%' && str[i + 1] == '%'))
-			x = print_char(str[i], print_len);
-		else if (str[i + 1] == 'c')
-			x = print_char(va_arg(args_ptr, unsigned int), print_len);
-		else if (str[i + 1] == 's')
-			x = print_string(va_arg(args_ptr, char *), print_len);
-		else if (str[i + 1] == 'p' || str[i + 1] == 'x' || str[i + 1] == 'X')
-			x = print_hexadec(va_arg(args_ptr, size_t), str[i + 1], print_len);
-		else if (str[i + 1] == 'd' || str[i + 1] == 'i' || str[i + 1] == 'u')
-			x = print_number(va_arg(args_ptr, int), str[i + 1], print_len);
-		else if (str[i + 1] == 'f')
-			x = print_float(va_arg(args_ptr, double), print_len);
-		else
-			return (-1);
-		if (x == -1)
-			return (-1);
-		if (str[i] == '%')
-			i++;
-		i++;
-	}
-	return (0);
-}
-
-static int	print_char(char c, int *print_len)
+int	print_char(char c, int *print_len)
 {
 	if (write(1, &c, 1) == -1)
 		return (-1);
@@ -59,7 +20,7 @@ static int	print_char(char c, int *print_len)
 	return (0);
 }
 
-static int	print_string(char *str, int *print_len)
+int	print_string(char *str, int *print_len)
 {
 	if (str == NULL)
 	{
@@ -76,7 +37,7 @@ static int	print_string(char *str, int *print_len)
 	return (0);
 }
 
-static int	print_number(int nbr, char c, int *print_len)
+int	print_number(int nbr, char c, int *print_len)
 {
 	if (c == 'd' || c == 'i')
 	{
@@ -93,14 +54,14 @@ static int	print_number(int nbr, char c, int *print_len)
 	return (0);
 }
 
-static int print_float(double nbr, int *print_len)
+int	print_float(double nbr, int *print_len)
 {
 	ft_putnbr_float(nbr, 6);
 	*print_len += count_digits((int)nbr) + 7;
 	return (0);
-}	
+}
 
-static int	print_hexadec(size_t nbr, char format, int *print_len)
+int	print_hexadec(size_t nbr, char format, int *print_len)
 {
 	int	x;
 
@@ -109,8 +70,8 @@ static int	print_hexadec(size_t nbr, char format, int *print_len)
 	{
 		if (nbr == 0)
 		{
-			*print_len += 5;
-			return (ft_putstr("(nil)"));
+			*print_len += 3;
+			return (ft_putstr("0x0"));
 		}
 		if (ft_putstr("0x") == -1)
 			return (-1);
--- ft_printf/ft_putnbr_float.c
+++ ft_printf/ft_putnbr_float.c
@@ -6,35 +6,36 @@
 /*   By: belinore <belinore@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 18:54:52 by belinore          #+#    #+#             */
-/*   Updated: 2025/03/21 20:48:37 by belinore         ###   ########.fr       */
+/*   Updated: 2025/03/31 21:39:05 by belinore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int    ft_putnbr_float(double num, int decimal_places)
+int	ft_putnbr_float(double num, int decimal_places)
 {
-    int     integer;
-    int     decimal;
+	int	integer;
+	int	decimal;
 
-    integer = (int)num;
-    if (ft_putnbr(integer) == -1)
-        return (-1);
-    if (write(1, ".", 1) == -1)
-        return (-1);
-    num = num - integer;
-    if (integer == 0 && num < 0 && write(1, "-", 1) == -1)
-        return (-1);
-    if (num < 0)
-        num = -num;
-    while (decimal_places > 0)
-    {
-        num *= 10;
-        decimal = (int)num;
-        if (ft_putnbr(decimal) == -1)
-            return (-1);
-        num = num - decimal;
-        decimal_places--;
-    }
-    return (0);
+	integer = (int)num;
+	if (ft_putnbr(integer) == -1)
+		return (-1);
+	if (decimal_places > 0)
+	{
+		if (write(1, ".", 1) == -1)
+			return (-1);
+		num = num - integer;
+		if (num < 0)
+			num = -num;
+		while (decimal_places > 0)
+		{
+			num *= 10;
+			decimal = (int)num;
+			if (ft_putnbr(decimal) == -1)
+				return (-1);
+			num = num - decimal;
+			decimal_places--;
+		}
+	}
+	return (0);
 }
